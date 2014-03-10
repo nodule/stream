5c3e756b0751ee043bd7d4b480d22fb75d412acf
Rob Halff <rob.halff@gmail.com>
2014-03-10 04:31:44 +0100

    add notes

--- nodes/onData/node.js
+++ nodes/onData/node.js
@@ -1,17 +1,25 @@
 output = function(cb) {
   // just an anonymous group now
   // can just be collect with >=
+  // TODO: sometimes the grouping behavior is wanted
+  // sometimes not, we do not want to group an
+  // ongoing stream..
   var g = chi.group();
+
   input.stream.on('data', function(chunk) {
+
     cb({
       out: chunk
     }, g.item());
+
   });
 
   input.stream.on('end', function(chunk) {
+
     if(chunk) {// not sure if needed
       cb({ out: chunk }, g.item());
     }
+
     g.done();
     done(); // triggers complete.
   }
