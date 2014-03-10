0258f2a5d0296d55136f9018296578e07b11b3ed
Rob Halff <rob.halff@gmail.com>
2014-03-10 04:29:07 +0100

    send along group data and autoclose

--- nodes/onData/node.js
+++ nodes/onData/node.js
@@ -1,5 +1,18 @@
 output = function(cb) {
+  // just an anonymous group now
+  // can just be collect with >=
+  var g = chi.group();
   input.stream.on('data', function(chunk) {
-    cb({ out: chunk } );
+    cb({
+      out: chunk
+    }, g.item());
   });
+
+  input.stream.on('end', function(chunk) {
+    if(chunk) {// not sure if needed
+      cb({ out: chunk }, g.item());
+    }
+    g.done();
+    done(); // triggers complete.
+  }
 };
